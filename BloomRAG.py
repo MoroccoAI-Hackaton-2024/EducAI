8fda81925cf65e41dc76caf0a03b7821f3a6969e
ErgodicEntropy <ayoubgameranger@gmail.com>
2024-11-28 19:17:18 +0100

    langchain init

---
+++ BloomRAG.py
@@ -0,0 +1,146 @@
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter 
+from langchain.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain import PromptTemplate
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from langchain.llms import CTransformers
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_community.llms import CTransformers
+import chainlit as cl
+from langchain.llms import huggingface_hub
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATA_PATH = 'data/'
+DB_FAISS_PATH = 'vectorstore/db_faiss'
+
+# Create vector database
+def create_vector_db():
+    loader = DirectoryLoader(DATA_PATH,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
+                                                   chunk_overlap=50)
+    texts = text_splitter.split_documents(documents)
+
+    embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                       model_kwargs={'device': 'cpu'})
+
+    db = FAISS.from_documents(texts, embeddings)
+    db.save_local(DB_FAISS_PATH)
+
+if __name__ == "__main__":
+    create_vector_db()
+
+
+
+def set_custom_prompt():
+    """
+    Prompt template for QA retrieval for each vectorstore
+    """
+    prompt =PromptTemplate(input_variables = ["question"], template ="""
+    You are an educational design assistant specializing in Bloom's Taxonomy. Your task is to transform a set of input questions into questions aligned with each level of Bloom's Taxonomy (Remember, Understand, Apply, Analyze, Evaluate, and Create). For every input question, generate a question for each level of the taxonomy, ensuring the new questions remain relevant to the topic of the original question. Structure your output as follows:
+
+Original Question: {question}
+Remember: [Question targeting recall of knowledge]
+Understand: [Question requiring comprehension]
+Apply: [Question involving practical application]
+Analyze: [Question prompting breakdown into components]
+Evaluate: [Question asking for judgment or critique]
+Create: [Question requiring synthesis or creation of new ideas]
+Example:
+Input Question: What are the causes of climate change?
+
+Remember: What is climate change, and what are its primary causes?
+Understand: How do greenhouse gases contribute to climate change?
+Apply: Can you identify the greenhouse gas emissions in your daily activities?
+Analyze: What are the key differences between natural and human-induced causes of climate change?
+Evaluate: How effective are current policies in mitigating climate change?
+Create: Propose a new strategy to reduce the effects of climate change on urban areas
+    """)
+    return prompt
+
+#Retrieval QA Chain
+def retrieval_qa_chain(llm, prompt, db):
+    qa_chain = RetrievalQA.from_chain_type(llm=llm,
+                                       chain_type='stuff',
+                                       retriever=db.as_retriever(search_kwargs={'k': 2}),
+                                       return_source_documents=True,
+                                       chain_type_kwargs={'prompt': prompt}
+                                       )
+    return qa_chain
+
+#Loading the model
+def load_llm():
+    # Load the locally downloaded model here
+    # llm = CTransformers(
+    #     model = "TheBloke/Llama-2-7B-Chat-GGML",
+    #     model_type="llama",
+    #     max_new_tokens = 512,
+    #     temperature = 0.5
+    # )
+    llm = huggingface_hub(
+    repo_id='declare-lab/flan-alpaca-large', #declare-lab/flan-alpaca-gpt4-xl
+    model_kwargs={'temperature': 0.2, 'max_length': 400}
+)
+    return llm
+
+#QA Model Function
+def qa_bot():
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2",
+                                       model_kwargs={'device': 'cpu'})
+    db = FAISS.load_local(DB_FAISS_PATH, embeddings)
+    llm = load_llm()
+    qa_prompt = set_custom_prompt()
+    qa = retrieval_qa_chain(llm, qa_prompt, db)
+
+    return qa
+
+#output function
+def final_result(query):
+    qa_result = qa_bot()
+    response = qa_result({'query': query})
+    return response
+
+#chainlit code
+@cl.on_chat_start
+async def start():
+    chain = qa_bot()
+    msg = cl.Message(content="Starting the bot...")
+    await msg.send()
+    msg.content = "Hi, Welcome to BloomAI. What is your question?"
+    await msg.update()
+
+    cl.user_session.set("chain", chain)
+
+@cl.on_message
+async def main(message: cl.Message):
+    chain = cl.user_session.get("chain") 
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
+    )
+    cb.answer_reached = True
+    res = await chain.acall(message.content, callbacks=[cb])
+    answer = res["result"]
+    # sources = res["source_documents"]
+
+    # if sources:
+    #     answer += f"\nSources:" + str(sources)
+    # else:
+    #     answer += "\nNo sources found"
+
+    await cl.Message(content=answer).send()
+
+
